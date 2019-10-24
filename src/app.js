704ded1f73ba4589bede94a7d342917647e88556
Aashna Prakash <aashna.prakash@sap.com>
2019-10-24 14:10:54 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const app = express();
+app.set('view engine','ejs');
+app.set('views', path.join(__dirname,'views'));
+app.use(express.static(path.join(__dirname,'public')));
+app.get('/',(err,res)=>{
+    res.render('index', {title: 'Index'});
+});
+app.listen(3000,()=>{
+    console.log('PS Project Runningon port 3000!');
+});
