809a20f69fd415e6024e56f216fc6dee43a110bd
Aashna Prakash <aashna.prakash@sap.com>
2019-10-25 15:19:42 +0530

    fixes

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.get('/transfer',(req,res) => {
 app.post('/transfer',(req,res) => {
     accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
     accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount,10);
-    writeJSON(accounts);
+    writeJSON();
     res.render('transfer', {message: 'Transfer Completed'});
 });
 
@@ -39,7 +39,7 @@ app.get('/payment',(req,res) => {
 app.post('/payment',(req,res) => {
     accounts.credit.balance = accounts.credit.balance - req.body.amount;
     accounts.credit.available = parseInt(accounts.credit.available) + parseInt(req.body.amount,10);
-    writeJSON(accounts);
+    writeJSON();
     res.render('payment', {message: 'Payment Completed'});
 });
 
--- src/data.js
+++ src/data.js
@@ -7,9 +7,9 @@ const accounts = JSON.parse(accountData);
 const userData = fs.readFileSync(path.join(__dirname,'json','users.json'),'utf8');
 const users = JSON.parse(userData);
 
-const writeJSON = (data) =>{
-    const accountsJSON = JSON.stringify(data, null, 4);
-    fs.writeFileSync(path.join(__dirname,'json','accounts.json'), accountsJSON, 'utf8');
+const writeJSON = () =>{
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname,'json/accounts.json'), accountsJSON, 'utf8');
 };
 
 module.exports = {accounts, users, writeJSON};
